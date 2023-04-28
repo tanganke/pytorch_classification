f7783036f762f41e5db693c286b96b9ee4a1aa6a
tanganke <tang.anke@foxmail.com>
2023-04-28 16:09:01 +0800

    update

---
+++ pytorch_classification/transforms/imagenet.py
@@ -0,0 +1,27 @@
+"""
+Standard ImageNet Argumentation
+
+https://github.com/pytorch/examples/blob/main/imagenet/main.py
+"""
+from torchvision import transforms
+
+to_tensor = transforms.ToTensor()
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+train_transform = transforms.Compose(
+    [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        to_tensor,
+        normalize,
+    ]
+)
+
+test_transform = val_transform = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        to_tensor,
+        normalize,
+    ]
+)
---
+++ pytorch_classification/transforms/mnist.py
@@ -0,0 +1,12 @@
+"""
+https://github.com/pytorch/examples/blob/main/mnist/main.py
+"""
+from torchvision import transforms
+
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ]
+)
