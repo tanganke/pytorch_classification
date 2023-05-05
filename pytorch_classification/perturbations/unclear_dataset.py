8b17747b75b3330020c7938220cec916aaa7c37c
tanganke <tang.anke@foxmail.com>
2023-05-05 15:45:14 +0800

    update

---
+++ pytorch_classification/perturbations/unclear_dataset.py
@@ -0,0 +1,59 @@
+"""
+Construct Unclear Dataset.
+
+    (ICLR 2021) Huang etc. Unlearnable Examples: Making Personal Data Unexploitable
+"""
+import random
+from typing import List, Optional
+
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+
+
+class SamplewiseUncleanDataset(Dataset):
+    def __init__(
+        self,
+        dataset: Dataset,
+        noise: Tensor,
+        perturb_ratio: float = 1,
+    ):
+        super().__init__()
+        assert len(dataset) == len(noise)
+        self.dataset = dataset
+        self.noise = noise
+        self.perburt_ratio = perturb_ratio
+
+    def __getitem__(self, index: int):
+        x, y = self.dataset[index]
+        if random.random() <= self.perburt_ratio:
+            x = (x + self.noise[index]).clamp(0, 1)
+        return x, y
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+class ClasswiseUncleanDataset(Dataset):
+    def __init__(
+        self,
+        dataset: Dataset,
+        noise: Tensor,
+        if_perturb: bool | List[bool] = True,
+        perturb_ratio: float = 1,
+    ):
+        super().__init__()
+        self.dataset = dataset
+        self.noise = noise
+        self.if_perturb = if_perturb
+        self.perburt_ratio = perturb_ratio
+
+    def __getitem__(self, index: int):
+        x, y = self.dataset[index]
+        if self.if_perturb or self.if_perturb[y]:
+            if random.random() <= self.perburt_ratio:
+                x = (x + self.noise[y]).clamp(0, 1)
+        return x, y
+
+    def __len__(self):
+        return len(self.dataset)
--- pytorch_classification/transforms/__init__.py
+++ pytorch_classification/transforms/__init__.py
@@ -1,3 +1,5 @@
+from torchvision import transforms
+
 from .cifar import test_transform as cifar_test_transform
 from .cifar import train_transform as cifar_train_transform
 from .cifar import val_transform as cifar_val_transform
@@ -5,3 +7,7 @@ from .imagenet import test_transform as imagenet_test_transform
 from .imagenet import train_transform as imagenet_train_transform
 from .imagenet import val_transform as imagenet_val_transform
 from .mnist import transform as mnist_transform
+
+
+to_tensor = transforms.ToTensor()
+to_pil_image = transforms.ToPILImage()
